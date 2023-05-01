fb367277d805441078dc98dc10bf86e957a7ea6b
sankarharidasmenon <sankar.haridas@gmail.com>
2023-05-01 12:52:02 +0530

    Add files via upload

---
+++ ms3.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class ms3 {
+
+    public static void main(String[] args)
+    {
+        String cmdb = "connecting to mongodb";
+        String dcmdb = "disconnecting to mongodb";
+        System.out.print(cmdb);
+
+        const { MongoClient, ServerApiVersion } = require('mongodb');
+        const uri = "mongodb+srv://sankarharidas:brAPRqBHLgbN8rq9@cluster0.mx4mbtb.mongodb.net/?retryWrites=true&w=majority";
+        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+        const client = new MongoClient(uri, {
+        serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+                    }
+                                            });
+        async function run() {
+        try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+         }
+        }
+        run().catch(console.dir);
+
+        }    
+}

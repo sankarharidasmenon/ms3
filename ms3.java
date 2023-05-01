380bea0ca8d933e16e5823947fee3a3fb62000fc
sankarharidasmenon <sankar.haridas@gmail.com>
2023-05-01 13:37:29 +0530

    Update ms3.java

--- ms3.java
+++ ms3.java
@@ -9,7 +9,7 @@ public class ms3 {
         System.out.print(cmdb);
 
         const { MongoClient, ServerApiVersion } = require('mongodb');
-        const uri = "mongodb+srv://sankarharidas:brAPRqBHLgbN8rq9@cluster0.mx4mbtb.mongodb.net/?retryWrites=true&w=majority";
+        const uri = "mongodb+srv://sankarharidas:password@cluster0.mx4mbtb.mongodb.net/?retryWrites=true&w=majority";
         // Create a MongoClient with a MongoClientOptions object to set the Stable API version
         const client = new MongoClient(uri, {
         serverApi: {
